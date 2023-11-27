ec0b7aa55ddf6386749a24ef7148073d1706d665
Mehmed <mehmed_duhovic@hotmail.com>
2023-11-27 20:46:07 +0100

    importing pantone colors, showing them as a list

--- src/Components/ColorSelectModal/ColorSelectModal.jsx
+++ src/Components/ColorSelectModal/ColorSelectModal.jsx
@@ -6,7 +6,10 @@ export default function ColorSelectModal() {
   const [showModal, setShowModal] = useState(false);
   return (
     <>
-      <button onClick={() => setShowModal(true)}>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full absolute top-0 left-[50%]"
+        onClick={() => setShowModal(true)}
+      >
         Open pantone selector
       </button>
       {showModal &&
--- src/Components/ModalContent.jsx
+++ src/Components/ModalContent.jsx
@@ -1,8 +1,34 @@
+import { pantoneColors } from "../config/pantone-colors";
+
 export default function ModalContent({ onClose }) {
+  const pantoneObjectColors = pantoneColors.names.map((name, index) => ({
+    colorName: name,
+    value: pantoneColors.values[index],
+  }));
+
+  console.log(pantoneObjectColors[0]);
   return (
-    <div className="modal">
-      <div>I'm a modal dialog</div>
-      <button onClick={onClose}>Close</button>
+    <div className="modal flex content-center flex-col w-[800px] h-[800px] z-index-2 overflow-auto">
+      <div className="flex items-center bg-indigo-100 flex-wrap">
+        {pantoneObjectColors.map((color) => {
+          return (
+            <div
+              className="pantone-color-box m-1 rounded-lg h-40 w-[150px] content-center cursor-pointer"
+              style={{ backgroundColor: color.value }}
+              key={color.colorName}
+            >
+              <div>{color.colorName}</div>
+              <div>{color.value}</div>
+            </div>
+          );
+        })}
+      </div>
+      <button
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        onClick={onClose}
+      >
+        Close
+      </button>
     </div>
   );
 }
