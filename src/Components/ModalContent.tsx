532992acd99ec95b799381e2d88f9fc5eedea2b3
Mehmed <mehmed_duhovic@hotmail.com>
2023-12-04 22:09:39 +0100

    adding new font, centering the modal

--- src/Components/ModalContent.tsx
+++ src/Components/ModalContent.tsx
@@ -7,12 +7,12 @@ const ModalContent: React.FC<{ onClose: () => void }> = ({ onClose }) => {
     value: pantoneColors.values[index],
   }));
   return (
-    <div className="modal flex content-center flex-col w-[800px] h-[800px] z-index-2 overflow-auto">
+    <div className="w-[800px] h-[800px] fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] modal flex content-center flex-col z-index-2 overflow-auto no-scrollbar">
       <div className="flex items-center bg-indigo-100 flex-wrap">
         {pantoneObjectColors.map((color) => {
           return (
             <div
-              className="pantone-color-box m-1 rounded-lg h-40 w-[150px] content-center cursor-pointer"
+              className="pantone-color-box m-1 rounded-lg h-[120px] w-[120px] content-center cursor-pointer"
               style={{ backgroundColor: color.value }}
               key={color.colorName}
             >
