462431825a75264b2b83d45242d2c1cb43299436
Mehmed <mehmed_duhovic@hotmail.com>
2023-12-27 00:18:02 +0100

    adding clear button, fixing fixed button

--- src/Components/ActionButtons.tsx
+++ src/Components/ActionButtons.tsx
@@ -1,15 +1,25 @@
 const ActionButtons = ({
   generateGradient,
+  clear,
 }: {
   generateGradient: () => void;
+  clear: () => void;
 }) => {
   return (
-    <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-[200px]"
-      onClick={() => generateGradient()}
-    >
-      Generate gradient
-    </button>
+    <div className="flex w-[450px] justify-around">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-[200px]"
+        onClick={() => generateGradient()}
+      >
+        Generate gradient
+      </button>
+      <button
+        className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-[200px]"
+        onClick={() => clear()}
+      >
+        Clear
+      </button>
+    </div>
   );
 };
 
--- src/Components/ModalContent.tsx
+++ src/Components/ModalContent.tsx
@@ -27,44 +27,55 @@ const ModalContent: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   };
 
   const generateGradientHandler = () => {
-    console.log("Test")
-  }
+    console.log("Test");
+  };
+
+  const clearHandler = () => {
+    setColorsSelected([]);
+  };
 
   return (
-    <div className="w-[800px] h-[800px] fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] modal flex content-center flex-col z-index-2 overflow-auto no-scrollbar border rounded-md shadow-md">
-      <div className="header w-100 block min-h-[60px]">
-        <button
-          onClick={onClose}
-          className="close absolute top-[10px] right-[10px] text-5xl"
-        >
-          &times;
-        </button>
+    <>
+      <div className="w-[800px] h-[800px] fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] modal flex content-center flex-col z-index-2 overflow-auto no-scrollbar border rounded-md shadow-md">
+        <div className="header w-100 block min-h-[60px]">
+          <button
+            onClick={onClose}
+            className="close absolute top-[10px] right-[10px] text-5xl"
+          >
+            &times;
+          </button>
+        </div>
+        <div className="flex justify-around flex-wrap">
+          {pantoneObjectColors.map((color) => {
+            return (
+              <div
+                onClick={() => setColor(color.value)}
+                className="relative pantone-color-box p-1 m-1 rounded-lg h-[100px] w-[100px] content-center cursor-pointer"
+                style={{
+                  backgroundColor: color.value,
+                }}
+                key={color.colorName}
+              >
+                {colorsSelected.includes(color.value) ? (
+                  <div className="w-[30px] h-[30px] top-[-6px] left-[-6px] text-center rounded-full bg-amber-300 absolute">
+                    {colorsSelected.indexOf(color.value) + 1}
+                  </div>
+                ) : null}
+                <ColorLabels color={color}></ColorLabels>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="flex justify-around flex-wrap">
-        {pantoneObjectColors.map((color) => {
-          return (
-            <div
-              onClick={() => setColor(color.value)}
-              className="relative pantone-color-box p-1 m-1 rounded-lg h-[100px] w-[100px] content-center cursor-pointer"
-              style={{
-                backgroundColor: color.value,
-              }}
-              key={color.colorName}
-            >
-              {colorsSelected.includes(color.value) ? (
-                <div className="w-[30px] h-[30px] top-[-6px] left-[-6px] text-center rounded-full bg-amber-300 absolute">
-                  {colorsSelected.indexOf(color.value) + 1}
-                </div>
-              ) : null}
-              <ColorLabels color={color}></ColorLabels>
-            </div>
-          );
-        })}
+      <div className="fixed bottom-[100px] left-[50%] translate-x-[-50%]">
+        {colorsSelected.length === 2 && (
+          <ActionButtons
+            generateGradient={generateGradientHandler}
+            clear={clearHandler}
+          />
+        )}
       </div>
-      <div className="fixed bottom-[20px] left-[50%] translate-x-[-50%]">
-        {colorsSelected.length === 2 && <ActionButtons generateGradient={generateGradientHandler} />}
-      </div>
-    </div>
+    </>
   );
 };
 
